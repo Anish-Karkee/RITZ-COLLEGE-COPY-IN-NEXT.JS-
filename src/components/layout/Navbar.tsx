3d5d6e6f10fb3366bec6dab693e2caa436cb3995
Anish-Karkee <anitakarkey7@gmail.com>
2026-06-29 15:12:36 +0545

    Add Navbar component and update page structure

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Phone, Mail } from 'lucide-react'
+import { Button } from '../ui/button'
+const Navbar = () => {
+  return (
+    <>
+      <div className=" bg-blue-900 text-white h-12 ">
+      <div className="container mx-auto flex items-center justify-between h-full ">
+        {/* telephone part  */}
+        <div className='flex items-center gap-2'>
+            <Phone size={18}/>
+            <span>025-587251</span>
+        </div>
+
+        {/*Mail part */}
+        <div className='flex items-center gap-2'>
+            <Mail size={18}/>
+            <span>ritzcollegeofhm@gmail.com</span>
+        </div>
+
+        {/*Right part */}
+        <Button className='bg-white text-blue-900 px-4 py-1 rounded font-medium hover:bg-gray-100'>
+            Apply Now
+        </Button>
+
+
+
+
+
+      </div>
+      </div>
+    </>
+  )
+}
+
+export default Navbar
