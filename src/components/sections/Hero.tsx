f6a5a64c3deee57c9a60b562674daf84a41246b0
Anish-Karkee <anitakarkey7@gmail.com>
2026-06-30 18:39:29 +0545

    feat: add overlay and button to Hero component

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import { Button } from "../ui/button"
 import LAND1 from "../../../public/images/landing1.jpg"
 
 const Hero = () => {
@@ -12,6 +13,15 @@ const Hero = () => {
             alt="image1"
             className="object-cover"
             />
+    </div>
+    {/*OVERLAY */}
+    <div className="absolute inset-0 bg-black/30"></div>
+
+    {/*Button */}
+    <div className="absolute bottom-1 left-1/2 -translate-x-1/2" >
+    <Button className="bg-blue-700 hover:bg-white hover:text-black text-white px-10 py-6 rounded-2xl text-2xl font-medium transition duration-300 shadow-xl ">
+      Read More
+    </Button>
     </div>
     </section>
 
